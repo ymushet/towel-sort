3c84e694ade633a0f9efc5cc933c22c2f023acae
Yehor Mushet <egor.mushet@gmail.com>
2021-03-07 15:23:39 +0200

    feat: sort with forEach, array.sort, and arr.flat()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if(!matrix) return [];
+    matrix
+        .forEach(function callback(currentValue, index, _) {
+            if (index % 2) {
+                currentValue.sort((a, b) => {
+                    return a <= b ? 1 : -1;
+                });
+            } else {
+                currentValue.sort((a, b) => {
+                    return a >= b ? 1 : -1;
+                });
+            }
+        })
+    return matrix.flat();
+};
